e5767c138a627fcc4f8df4bd313069fe97d50db1
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-05-21 23:58:55 +0100

    multiple changes

--- R/bcal.R
+++ R/bcal.R
@@ -1,29 +1,58 @@
 
-bcal <- function(p, l = NULL) { # P-Value Calibration from Selke, Bayarri & Berger (2001)
-# Calibrates p-values into lower bounds for Bayes Factors in favour of the null
+#' Calibration of P-Values for Testing Precise Null Hypotheses.
+#'
+#' \code{bcal} calibrates p-values under a robust Bayesian perspective so that they can be interpreted as the lower bound on the odds provided by the data (Bayes factor) in favor of a precise null hypothesis.
+#'
+#' A useful way to calibrate a p-value under a robust Bayesian perspective is by using the bound that is found as the minimum Bayes factor in favour of the null that is obtained by changing priors over large classes of distributions under \eqn{H_{1}{H1}}. This Bayesian calibration is obtained with \deqn{B(p) = -exp(1) p log (p)} when \eqn{p < 1/exp{1}}  \insertCite{pericchiTorres2011}{pcal}  \insertCite{sellke2001}{pcal}
+#'
+#' @param p A numeric vector. All elements must be between zero and one (inclusive).
+#'
+#' @return Returns a numeric vector with length equal to \code{length(p)}
 
-  if (missing(l)){
+#' @examples
+#'
+#' # Calibration of p-values
+#'
+#' round(bcal(c(.1, .05, .01)), 3)
+#'
+#' df <- read.csv("https://goo.gl/j6lRXD")
+#' table(df$treatment, df$improvement)
+#' bcal(chisq.test(df$treatment, df$improvement, correct=FALSE)[["p.value"]])
+#'
+#' @importFrom Rdpack reprompt
+#' @seealso \code{\link{pcal}}
+#' @export
 
-    ifelse(p>1, stop("p>1", call. = FALSE),
-           ifelse(p<0, stop("p<0", call. = FALSE),
-                  ifelse(p==0, 0,
-                         ifelse(p < (1 / exp(1)), -1*exp(1)*p*log(p), 1)
-                  )
-           )
-    )
 
-  } else  {
 
-    ifelse(p>1, stop("p>1", call. = FALSE),
-           ifelse(p<0, stop("p<0", call. = FALSE),
-                  ifelse(p==0, log(0, base=l),
-                         ifelse(p < (1 / exp(1)), log(-1*exp(1)*p*log(p),base = l), log(1, base=l))
-                  )
-           )
-    )
+bcal <- function(p) {
 
+  if(is.null(p)){
+    stop("Invalid argument: 'p' is NULL")
   }
+  if(!is.numeric(p)){
+    stop("Invalid argument: 'p' must be a numeric vector")
+  }
+  if(any(p > 1)){
+    stop("Invalid argument: 'p' > 1")
+  }
+  if(any(p < 0)){
+    stop("Invalid argument: 'p' < 0")
+  }
+  if(all(is.na(p))){
+    warning("All elements of 'p' are NA")
+  }
+  if(any(is.na(p))){
+    warning("Some elements of 'p' are NA")
+  }
+
+  ifelse(p == 0, 0,
+         ifelse(p < (exp(1) ^ (-1)), -exp(1) * log(p) * p,
+                1))
+
 }
+
+
 
 
 
--- R/pcal.R
+++ R/pcal.R
@@ -1,13 +1,52 @@
 
-pcal <- function(p, prob = 0.5){ # P-Value Calibrations from Selke, Bayarri & Berger (2001)
-  # Calibrates p-values into lower bounds for posterior probability of the null.
+#' Calibration of P-Values for Testing Precise Null Hypotheses
+#'
+#' \code{pcal} calibrates p-values under a robust Bayesian perspective so that they can be interpreted in either a Bayesian or a frequentist way.
+#'
+#' A useful way to calibrate a p-value under a robust Bayesian perspective is by using the bound that is found as the minimum posterior probability of \eqn{H_0} that is obtained by changing priors over large classes of priors under \eqn{H_{1}{H1}}.
+#'
+#' @param pi_null A numeric of length one value between zero and one.
+#'
+#' @examples
+#' round(pcal(c(.1, .05, .01)), 3)
+#' df <- read.csv("https://goo.gl/j6lRXD")
+#' table(df$treatment, df$improvement)
+#'
+#' pcal(chisq.test(df$treatment, df$improvement, correct=FALSE)[["p.value"]])
+#'
+#' @seealso \code{\link{bcal}}
+#' @importFrom Rdpack reprompt
+#' @export
+#'
+pcal <- function(p, pi_null = 0.5){
 
-  ifelse(p > 1 , stop("p>1 -- Invalid p-value.", call. = FALSE),
-         ifelse(p < 0, stop("p<0 -- invalid p-value.", call. = FALSE),
-                ifelse(p == 0, 0,
-                       ifelse(p<(1/exp(1)), return((1+((1-prob)/prob)*(1/bcal(p)))^(-1)),
-                              return((1+((1-prob)/prob)*(1/bcal(p)))^(-1)))
-                )
-         )
-  )
+  if(is.null(p)){
+    stop("Invalid argument: 'p' NULL")
+  }
+  if(all(is.na(p))){
+    stop("Invalid argument: all elements of 'p' are NA")
+  }
+  if(!is.numeric(p)){
+    stop("Invalid argument: 'p' must be a numeric vector")
+  }
+  if(is.na(pi_null)){
+    stop("Invalid argument: 'pi_null' is NA")
+  }
+  if(is.null(pi_null)){
+    stop("Invalid argument: 'pi_null' NULL")
+  }
+  if(any(!is.numeric(pi_null), isFALSE(length(pi_null) == 1))){
+    stop("Invalid argument: 'pi_null' must be a numeric vector of length 1")
+  }
+
+  ifelse(p == 0, 0, bfactor_to_prob(bcal(p)))
+
 }
+
+
+
+
+
+
+
+
