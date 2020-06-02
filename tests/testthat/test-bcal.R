e099f3363f5e8e6d70b96e98aff13242b0bab5a6
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-06-02 23:39:36 +0100

    New tests

--- tests/testthat/test-bcal.R
+++ tests/testthat/test-bcal.R
@@ -24,3 +24,83 @@ testthat::test_that("bcal 0.001", {
   )
 }
 )
+
+testthat::test_that("bcal w/ NULL", {
+  testthat::expect_error(
+    bcal(NULL)
+  )
+}
+)
+
+testthat::test_that("bcal empty vector", {
+  testthat::expect_error(
+    bcal(vector())
+  )
+}
+)
+
+testthat::test_that("bcal w/ empty list", {
+  testthat::expect_error(
+    bcal(list())
+  )
+}
+)
+
+testthat::test_that("bcal w/ list", {
+  testthat::expect_error(
+    bcal(list(1))
+  )
+}
+)
+
+testthat::test_that("bcal w/ char", {
+  testthat::expect_error(
+    bcal("test")
+  )
+}
+)
+
+testthat::test_that("bcal w/ factor", {
+  testthat::expect_error(
+    bcal(factor(1))
+  )
+}
+)
+
+testthat::test_that("bcal w/ p<0", {
+  testthat::expect_error(
+    bcal(c(.1, -.1))
+  )
+}
+)
+
+testthat::test_that("bcal w/ p>1", {
+  testthat::expect_error(
+    bcal(c(.1, 1.1))
+  )
+}
+)
+
+testthat::test_that("bcal NA warning", {
+  testthat::expect_warning(
+    bcal(c(.1, NA))
+  )
+}
+)
+
+testthat::test_that("bcal NaN warning", {
+  testthat::expect_warning(
+    bcal(c(.1, NaN))
+  )
+}
+)
+
+
+
+
+
+
+
+
+
+
--- tests/testthat/test-pcal.R
+++ tests/testthat/test-pcal.R
@@ -47,6 +47,168 @@ testthat::test_that("pcal bl2", {
 }
 )
 
+testthat::test_that("pcal w/ NULL p", {
+  testthat::expect_error(
+    pcal(NULL)
+  )
+}
+)
+
+testthat::test_that("pcal p empty vector", {
+  testthat::expect_error(
+    pcal(vector())
+  )
+}
+)
+
+testthat::test_that("pcal p empty list", {
+  testthat::expect_error(
+    pcal(list())
+  )
+}
+)
+
+testthat::test_that("pcal p w/ list", {
+  testthat::expect_error(
+    pcal(list(1))
+  )
+}
+)
+
+testthat::test_that("pcal p w/ char", {
+  testthat::expect_error(
+    pcal("test")
+  )
+}
+)
+
+testthat::test_that("pcal p w/ factor", {
+  testthat::expect_error(
+    pcal(factor(1))
+  )
+}
+)
+
+testthat::test_that("pcal p w/ p<0", {
+  testthat::expect_error(
+    pcal(c(.1, -.1))
+  )
+}
+)
+
+testthat::test_that("pcal p w/ p>1", {
+  testthat::expect_error(
+    pcal(c(.1, 1.1))
+  )
+}
+)
+
+testthat::test_that("pcal p NA warning", {
+  testthat::expect_warning(
+    pcal(c(.1, NA))
+  )
+}
+)
+
+testthat::test_that("pcal p NaN warning", {
+  testthat::expect_warning(
+    pcal(c(.1, NaN))
+  )
+}
+)
+
+testthat::test_that("pcal NULL prior_prob", {
+  testthat::expect_error(
+    pcal(.1, NULL)
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob empty vector", {
+  testthat::expect_error(
+    pcal(.1, vector())
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob - empty list ", {
+  testthat::expect_error(
+    pcal(.1, list())
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob - NA ", {
+  testthat::expect_error(
+    pcal(.1, NA)
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob - NaN ", {
+  testthat::expect_error(
+    pcal(.1, NaN)
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob - char ", {
+  testthat::expect_error(
+    pcal(.1, "test")
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob - factor ", {
+  testthat::expect_error(
+    pcal(.1, factor(.1))
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob - list ", {
+  testthat::expect_error(
+    pcal(.1, list(.1))
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob > 1 ", {
+  testthat::expect_error(
+    pcal(.1, 1.1)
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob < 1 ", {
+  testthat::expect_error(
+    pcal(.1, -.1)
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob length t1", {
+  testthat::expect_error(
+    pcal(c(.1, .1, .1, .1), c(.1, .2, .3))
+  )
+}
+)
+
+testthat::test_that("pcal prior_prob length t2 ", {
+  testthat::expect_error(
+    pcal(c(.1, .1), c(.1, .2, .3))
+  )
+}
+)
+
+
+
+
+
+
+
+
+
 
 
 
