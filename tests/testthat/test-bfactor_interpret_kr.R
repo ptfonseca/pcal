207b4ed7039db3311d3fdbd99e98e7f7da460ce8
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-06-26 20:11:30 +0100

    New tests

--- tests/testthat/test-bfactor_interpret_kr.R
+++ tests/testthat/test-bfactor_interpret_kr.R
@@ -74,3 +74,87 @@ testthat::test_that("bfactor_interpret_kr NA warning", {
     bfactor_interpret_kr(c(10, NA))
   )}
 )
+
+context("bfactor_log_interpret_kr")
+
+testthat::test_that("bfactor_log_interpret_kr error message 1", {
+  expect_error(
+    bfactor_log_interpret_kr(NULL)
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr error message 2 - empty vector", {
+  expect_error(
+    bfactor_log_interpret_kr(vector())
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr error message 2 - empty list", {
+  expect_error(
+    bfactor_log_interpret_kr(list())
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr error message 3 - NA", {
+  expect_error(
+    bfactor_log_interpret_kr(NA)
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr error message 3 - NaN", {
+  expect_error(
+    bfactor_log_interpret_kr(NaN)
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr error message 4 - factor", {
+  expect_error(
+    bfactor_log_interpret_kr(factor(10))
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr error message 4 - char", {
+  expect_error(
+    bfactor_log_interpret_kr("10")
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr error message 4 - list", {
+  expect_error(
+    bfactor_log_interpret_kr(list(10))
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr base arg NA warning", {
+  expect_warning(
+    bfactor_log_interpret_kr(c(10, NA))
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr base arg error message 1", {
+  expect_error(bfactor_log_interpret_kr(bf = .2, base = NULL))}
+)
+
+testthat::test_that("bfactor_log_interpret_kr base arg error message 2", {
+  expect_error(
+    bfactor_log_interpret_kr(bf = .2, base = NA)
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr base arg error message 3", {
+  expect_error(
+    bfactor_log_interpret_kr(bf = .2, base = c(10, 10))
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr base arg error message 4", {
+  expect_error(
+    bfactor_log_interpret_kr(bf = .2, base = factor(10))
+  )}
+)
+
+testthat::test_that("bfactor_log_interpret_kr base arg error message 5", {
+  expect_error(
+    bfactor_log_interpret_kr(bf = .2, base = "10")
+  )}
+)
