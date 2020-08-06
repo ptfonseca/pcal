2b6b8186283340af52ee7f45a2fa952689cdfb69
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-06 20:56:09 +0100

    update documentation

--- R/check-functions.R
+++ R/check-functions.R
@@ -1,7 +1,7 @@
 
 #' @title Check if an object is a vector of valid probability values
 #'
-#' @description `check_prob` checks if an object is a numeric vector of valid probability values. This can be useful to validate inputs and intermediate calculations in user-defined functions or to validate the output of calculations/functions.
+#' @description `check_prob` checks if an object is a numeric vector of valid probability values. This can be useful to validate inputs or intermediate calculations in user-defined functions or to validate the output of calculations/functions.
 #'
 #' @param p An arbitrary object.
 #'
@@ -14,7 +14,7 @@
 #' @return `check_prob` does not return any output. There are three possible scenarios:
 #' * The call is silent if `p` is a numeric vector of valid probability values and there are no `NA` or `NaN` values.
 #' * An informative warning message is given if `p` is a numeric vector of valid probability values and there are `NA` or `NaN` values.
-#' * An informative error message is thrown if `p` is not a numeric vector of valid probability values.
+#' * An informative error message is thrown if `p` is not a numeric vector of valid probability values. This will \code{\link[base]{stop}} the execution (for example, when using `check_prob` to validate the inputs of user defined functions).
 #'
 #' @seealso
 #' * \code{\link[pcal]{check_bf}} To check if an object is a numeric vector of valid Bayes factor values.
@@ -67,10 +67,48 @@ check_prob <- function(p){
 
 #' @title Check if an object is a numeric vector of valid Bayes factor values
 #'
-#' @description `check_bf` checks if an object is a numeric vector of valid Bayes factor values. This can be useful to validate inputs and intermediate calculations in user-defined functions or to validate the output of calculations/functions.
+#' @description `check_bf` checks if an object is a numeric vector of valid Bayes factor values. This can be useful to validate inputs or intermediate calculations in user-defined functions or to validate the output of calculations/functions.
 #'
 #' @param bf An arbitrary object.
 #'
+#' @details `check_bf` conducts a series of tests to check if `bf` is a numeric vector of valid Bayes factor values. Namely, `check_bf` checks if:
+#' * `bf` is `NULL` or empty.
+#' * `bf` is a numeric (atomic) vector.
+#' * `bf` has `NA` or `NaN` values.
+#' *  The values of `bf` are non-negative.
+#'
+#' @return `check_bf` does not return any output. There are three possible scenarios:
+#' * The call is silent if `bf` is a numeric vector of valid Bayes factor values and there are no `NA` or `NaN` values.
+#' * An informative warning message is given if `p` is a numeric vector of valid Bayes factor values and there are `NA` or `NaN` values.
+#' * An informative error message is thrown if `p` is not a numeric vector of valid Bayes factor values. This will \code{\link[base]{stop}} the execution (for example, when using `check_bf` to validate the inputs of user defined functions).
+#'
+#' @seealso
+#' * \code{\link[pcal]{check_log_bf}} To check if an object is a numeric vector of valid logarithmic Bayes factor values.
+#' * \code{\link[pcal]{check_prob}} To check if an object is a numeric vector of valid probability values.
+#'
+#' @examples
+#' # Calls that pass silently:
+#' check_bf(0)
+#' check_bf(0.5)
+#' check_bf(100)
+#' check_bf(c(0, 0.5, 1, 10, 50, 100))
+#'
+#' # Call that throws an informative warning message:
+#' \dontrun{check_bf(c(0.1, 0.2, NA, 0.4, 0.5))}
+#'
+#' # Calls that throw informative error messages:
+#' \dontrun{check_bf(NULL)}
+#' \dontrun{check_bf()}
+#' \dontrun{check_bf(TRUE)}
+#' \dontrun{check_bf("0.5")}
+#' \dontrun{check_bf(factor(0.5))}
+#' \dontrun{check_bf(matrix(0.5))}
+#' \dontrun{check_bf(list(0.5))}
+#' \dontrun{check_bf(NA)}
+#' \dontrun{check_bf(NaN)}
+#' \dontrun{check_bf(-0.5)}
+#' \dontrun{check_bf(c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5))}
+#' \dontrun{check_bf(-5)}
 #' @export
 
 check_bf <- function(bf){
@@ -97,9 +135,48 @@ check_bf <- function(bf){
 
 #' @title Check if an object is a numeric vector of valid logarithmic Bayes factor values
 #'
-#' @description `check_log_bf` checks if an object is a numeric vector of valid logarithmic Bayes factor values. This can be useful to validate inputs and intermediate calculations in user-defined functions or to validate the output of calculations/functions.
+#' @description `check_log_bf` checks if an object is a numeric vector of valid logarithmic Bayes factor values. This can be useful to validate inputs or intermediate calculations in user-defined functions or to validate the output of calculations/functions.
 #'
 #' @param bf An arbitrary object.
+#'
+#' @details `check_log_bf` conducts a series of tests to check if `bf` is a numeric vector of valid logarithmic Bayes factor values. Namely, `check_bf` checks if:
+#' * `bf` is `NULL` or empty.
+#' * `bf` is a numeric (atomic) vector.
+#' * `bf` has `NA` or `NaN` values.
+#'
+#' @return `check_bf` does not return any output. There are three possible scenarios:
+#' * The call is silent if `bf` is a numeric vector of valid Bayes factor values and there are no `NA` or `NaN` values.
+#' * An informative warning message is given if `p` is a numeric vector of valid Bayes factor values and there are `NA` or `NaN` values.
+#' * An informative error message is thrown if `p` is not a numeric vector of valid Bayes factor values.
+#'
+#' @seealso
+#' * \code{\link[pcal]{check_bf}} To check if an object is a numeric vector of valid Bayes factor values.
+#' * \code{\link[pcal]{check_prob}} To check if an object is a numeric vector of valid probability values.
+#'
+#' @examples
+#' # Calls that pass silently:
+#' check_log_bf(0)
+#' check_log_bf(0.5)
+#' check_log_bf(100)
+#' check_log_bf(c(0, 0.5, 1, 10, 50, 100))
+#' check_log_bf(-0.5)
+#' check_log_bf(-5)
+#' check_log_bf(c(-0.9, 0, 0.1, 0.2, 0.3, 0.4, 0.5))
+#'
+#' # Call that throws an informative warning message:
+#' \dontrun{check_log_bf(c(0.1, 0.2, NA, 0.4, 0.5))}
+#'
+#' # Calls that throw informative error messages:
+#' \dontrun{check_log_bf(NULL)}
+#' \dontrun{check_log_bf()}
+#' \dontrun{check_log_bf(TRUE)}
+#' \dontrun{check_log_bf("0.5")}
+#' \dontrun{check_log_bf(factor(0.5))}
+#' \dontrun{check_log_bf(matrix(0.5))}
+#' \dontrun{check_log_bf(list(0.5))}
+#' \dontrun{check_log_bf(NA)}
+#' \dontrun{check_log_bf(NaN)}
+#'
 #' @export
 
 check_log_bf <- function(bf){
