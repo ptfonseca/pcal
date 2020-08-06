c03bd3e55c9f3571ebafad2fa20c0fabea92b9f1
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-06 05:26:08 +0100

    New unit tests

--- tests/testthat/test-bcal.R
+++ tests/testthat/test-bcal.R
@@ -1,95 +1,95 @@
 
-testthat::context("bcal")
+context("bcal")
 
-testthat::test_that("bcal table", {
-  testthat::expect_equal(
+test_that("bcal table", {
+  expect_equal(
     round(bcal(c(0.2, 0.1, 0.05, 0.01)), 3),
     c(0.875, 0.626, 0.407, 0.125)
   )
 }
 )
 
-testthat::test_that("bcal 0.005", {
-  testthat::expect_equal(
+test_that("bcal 0.005", {
+  expect_equal(
     round(bcal(0.005), 3),
     0.072
   )
 }
 )
 
-testthat::test_that("bcal 0.001", {
-  testthat::expect_equal(
+test_that("bcal 0.001", {
+  expect_equal(
     round(bcal(0.001), 4),
     0.0188
   )
 }
 )
 
-testthat::test_that("bcal w/ NULL", {
-  testthat::expect_error(
+test_that("bcal w/ NULL", {
+  expect_error(
     bcal(NULL)
   )
 }
 )
 
-testthat::test_that("bcal empty vector", {
-  testthat::expect_error(
+test_that("bcal empty vector", {
+  expect_error(
     bcal(vector())
   )
 }
 )
 
-testthat::test_that("bcal w/ empty list", {
-  testthat::expect_error(
+test_that("bcal w/ empty list", {
+  expect_error(
     bcal(list())
   )
 }
 )
 
-testthat::test_that("bcal w/ list", {
-  testthat::expect_error(
+test_that("bcal w/ list", {
+  expect_error(
     bcal(list(1))
   )
 }
 )
 
-testthat::test_that("bcal w/ char", {
-  testthat::expect_error(
+test_that("bcal w/ char", {
+  expect_error(
     bcal("test")
   )
 }
 )
 
-testthat::test_that("bcal w/ factor", {
-  testthat::expect_error(
+test_that("bcal w/ factor", {
+  expect_error(
     bcal(factor(1))
   )
 }
 )
 
-testthat::test_that("bcal w/ p<0", {
-  testthat::expect_error(
+test_that("bcal w/ p<0", {
+  expect_error(
     bcal(c(.1, -.1))
   )
 }
 )
 
-testthat::test_that("bcal w/ p>1", {
-  testthat::expect_error(
+test_that("bcal w/ p>1", {
+  expect_error(
     bcal(c(.1, 1.1))
   )
 }
 )
 
-testthat::test_that("bcal NA warning", {
-  testthat::expect_warning(
+test_that("bcal NA warning", {
+  expect_warning(
     bcal(c(.1, NA))
   )
 }
 )
 
-testthat::test_that("bcal NaN warning", {
-  testthat::expect_warning(
+test_that("bcal NaN warning", {
+  expect_warning(
     bcal(c(.1, NaN))
   )
 }
--- tests/testthat/test-bfactor_interpret.R
+++ tests/testthat/test-bfactor_interpret.R
@@ -1,7 +1,7 @@
 
 context("bfactor_interpret")
 
-testthat::test_that("bfactor_interpret test 1", {
+test_that("bfactor_interpret test 1", {
 expect_equal(
  bfactor_interpret(10 ^ c(-3.10, -1.78, 1.06, -1.40, 1.21, 0.89, -2.37, 1.23, -8.88, 3.81, -8.38, 0.62)),
     c(
@@ -21,7 +21,7 @@ expect_equal(
   )
 })
 
-testthat::test_that("bfactor_interpret test 2", {
+test_that("bfactor_interpret test 2", {
   expect_equal(
     bfactor_interpret(10 ^ c(0.07, 1.29, 1.32, -0.62, -1.78, 1.55, -3.02, 1.25, 1.48)),
     c(
@@ -38,61 +38,61 @@ testthat::test_that("bfactor_interpret test 2", {
   )
 })
 
-testthat::test_that("bfactor_interpret error message 1", {
+test_that("bfactor_interpret error message 1", {
   expect_error(
     bfactor_interpret(NULL)
     )}
 )
 
-testthat::test_that("bfactor_interpret error message 2 - empty vector", {
+test_that("bfactor_interpret error message 2 - empty vector", {
   expect_error(
     bfactor_interpret(vector())
   )}
 )
 
-testthat::test_that("bfactor_interpret error message 2 - empty list", {
+test_that("bfactor_interpret error message 2 - empty list", {
   expect_error(
     bfactor_interpret(list())
   )}
 )
 
-testthat::test_that("bfactor_interpret error message 3 - NA", {
+test_that("bfactor_interpret error message 3 - NA", {
   expect_error(
     bfactor_interpret(NA)
   )}
 )
 
-testthat::test_that("bfactor_interpret error message 3 - NaN", {
+test_that("bfactor_interpret error message 3 - NaN", {
   expect_error(
     bfactor_interpret(NaN)
   )}
 )
 
-testthat::test_that("bfactor_interpret error message 4 - factor", {
+test_that("bfactor_interpret error message 4 - factor", {
   expect_error(
     bfactor_interpret(factor(10))
   )}
 )
 
-testthat::test_that("bfactor_interpret error message 4 - char", {
+test_that("bfactor_interpret error message 4 - char", {
   expect_error(
     bfactor_interpret("10")
   )}
 )
 
-testthat::test_that("bfactor_interpret error message 4 - list", {
+test_that("bfactor_interpret error message 4 - list", {
   expect_error(
     bfactor_interpret(list(10))
   )}
 )
 
-testthat::test_that("bfactor_interpret error message 5", {
+test_that("bfactor_interpret error message 5", {
   expect_error(
     bfactor_interpret(-0.6)
   )}
 )
 
-testthat::test_that("bfactor_interpret NA warning", {
+test_that("bfactor_interpret NA warning", {
   expect_warning(
     bfactor_interpret(c(10, NA))
   )}
@@ -100,7 +100,7 @@ testthat::test_that("bfactor_interpret NA warning", {
 
 context("bfactor_log_interpret")
 
-testthat::test_that("bfactor_log_interpret test 1", {
+test_that("bfactor_log_interpret test 1", {
   expect_equal(
     bfactor_log_interpret(c(-3.10, -1.78, 1.06, -1.40, 1.21, 0.89, -2.37, 1.23, -8.88, 3.81, -8.38, 0.62), base = 10),
     c(
@@ -120,7 +120,7 @@ testthat::test_that("bfactor_log_interpret test 1", {
   )
 })
 
-testthat::test_that("bfactor_log_interpret test 2", {
+test_that("bfactor_log_interpret test 2", {
   expect_equal(
     bfactor_log_interpret(c(0.07, 1.29, 1.32, -0.62, -1.78, 1.55, -3.02, 1.25, 1.48), base = 10),
     c(
@@ -137,77 +137,77 @@ testthat::test_that("bfactor_log_interpret test 2", {
   )
 })
 
-testthat::test_that("bfactor_log_interpret error message 1", {
+test_that("bfactor_log_interpret error message 1", {
   expect_error(
     bfactor_log_interpret(NULL)
   )}
 )
 
-testthat::test_that("bfactor_log_interpret error message 2 - empty vector", {
+test_that("bfactor_log_interpret error message 2 - empty vector", {
   expect_error(
     bfactor_log_interpret(vector())
   )}
 )
 
-testthat::test_that("bfactor_log_interpret error message 2 - empty list", {
+test_that("bfactor_log_interpret error message 2 - empty list", {
   expect_error(
     bfactor_log_interpret(list())
   )}
 )
 
-testthat::test_that("bfactor_log_interpret error message 3 - NA", {
+test_that("bfactor_log_interpret error message 3 - NA", {
   expect_error(
     bfactor_log_interpret(NA)
   )}
 )
 
-testthat::test_that("bfactor_log_interpret error message 3 - NaN", {
+test_that("bfactor_log_interpret error message 3 - NaN", {
   expect_error(
     bfactor_log_interpret(NaN)
   )}
 )
 
-testthat::test_that("bfactor_log_interpret error message 4 - factor", {
+test_that("bfactor_log_interpret error message 4 - factor", {
   expect_error(
     bfactor_log_interpret(factor(10))
   )}
 )
 
-testthat::test_that("bfactor_log_interpret error message 4 - char", {
+test_that("bfactor_log_interpret error message 4 - char", {
   expect_error(
     bfactor_log_interpret("10")
   )}
 )
 
-testthat::test_that("bfactor_log_interpret error message 4 - list", {
+test_that("bfactor_log_interpret error message 4 - list", {
   expect_error(
     bfactor_log_interpret(list(10))
   )}
 )
 
-testthat::test_that("bfactor_log_interpret base arg NA warning", {
+test_that("bfactor_log_interpret base arg NA warning", {
   expect_warning(
     bfactor_log_interpret(c(10, NA))
   )}
 )
 
-testthat::test_that("bfactor_log_interpret base arg error message 1", {
+test_that("bfactor_log_interpret base arg error message 1", {
   expect_error(bfactor_log_interpret(bf = .2, base = NULL))}
 )
 
-testthat::test_that("bfactor_log_interpret base arg error message 2", {
+test_that("bfactor_log_interpret base arg error message 2", {
   expect_error(
     bfactor_log_interpret(bf = .2, base = NA)
   )}
 )
 
-testthat::test_that("bfactor_log_interpret base arg error message 3", {
+test_that("bfactor_log_interpret base arg error message 3", {
   expect_error(
     bfactor_log_interpret(bf = .2, base = c(10, 10))
   )}
 )
 
-testthat::test_that("bfactor_log_interpret base arg error message 4", {
+test_that("bfactor_log_interpret base arg error message 4", {
   expect_error(
     bfactor_log_interpret(bf = .2, base = factor(10))
   )}
