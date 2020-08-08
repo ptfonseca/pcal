a415bd729358abcdf22649cca821d6cf34844645
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-08 23:13:37 +0100

    fix typo

--- tests/testthat/test-check_.R
+++ tests/testthat/test-check_.R
@@ -43,8 +43,6 @@ test_that("check error message 7", {expect_error(check_bf(NA))})
 test_that("check error message 8", {expect_error(check_bf(NaN))})
 test_that("check error message 12", {expect_error(check_bf())})
 
-context("check_bf")
-
 
 
 
