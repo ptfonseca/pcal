abf65cf7076de4f81126b7e0f5aadeeabaed843b
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-12 03:50:15 +0100

    update tests

---
+++ tests/testthat/test-bfactor-interpret.R
@@ -0,0 +1,215 @@
+
+context("bfactor_interpret jeffreys")
+
+test_that("bfactor_interpret scale argument", {
+  expect_equal(
+    bfactor_interpret(10 ^ c(-3.10, -1.78, 1.06, -1.40, 1.21, 0.89, -2.37, 1.23, -8.88, 3.81, -8.38, 0.62)),
+    bfactor_interpret(10 ^ c(-3.10, -1.78, 1.06, -1.40, 1.21, 0.89, -2.37, 1.23, -8.88, 3.81, -8.38, 0.62), scale = "jeffreys")
+  )
+})
+
+test_that("bfactor_interpret scale case sensitiveness", {
+  expect_equal(
+    bfactor_interpret(10 ^ c(-3.10, -1.78, 1.06, -1.40, 1.21, 0.89, -2.37, 1.23, -8.88, 3.81, -8.38, 0.62), scale = "Jeffreys"),
+    bfactor_interpret(10 ^ c(-3.10, -1.78, 1.06, -1.40, 1.21, 0.89, -2.37, 1.23, -8.88, 3.81, -8.38, 0.62), scale = "jeffreys")
+  )
+})
+
+test_that("bfactor_interpret test 1", {
+  expect_equal(
+    bfactor_interpret(10 ^ c(-3.10, -1.78, 1.06, -1.40, 1.21, 0.89, -2.37, 1.23, -8.88, 3.81, -8.38, 0.62)),
+    c(
+      "Negative",
+      "Negative",
+      "Strong",
+      "Negative",
+      "Strong",
+      "Substantial",
+      "Negative",
+      "Strong",
+      "Negative",
+      "Decisive",
+      "Negative",
+      "Substantial"
+    )
+  )
+})
+
+test_that("bfactor_interpret test 2", {
+  expect_equal(
+    bfactor_interpret(10 ^ c(0.07, 1.29, 1.32, -0.62, -1.78, 1.55, -3.02, 1.25, 1.48)),
+    c(
+      "Weak",
+      "Strong",
+      "Strong",
+      "Negative",
+      "Negative",
+      "Very Strong",
+      "Negative",
+      "Strong",
+      "Strong"
+    )
+  )
+})
+
+test_that("bfactor_interpret decisive", {
+  expect_equal(
+    bfactor_interpret(200),
+    "Decisive"
+  )
+})
+
+test_that("bfactor_interpret error message 1 test 2", {
+  expect_error(
+    bfactor_interpret(NULL)
+  )}
+)
+
+test_that("bfactor_interpret error message 1 test 2", {
+  expect_error(
+    bfactor_interpret(NULL, scale = "jeffreys")
+  )}
+)
+
+test_that("bfactor_interpret error message 2 - empty vector", {
+  expect_error(
+    bfactor_interpret(vector())
+  )}
+)
+
+test_that("bfactor_interpret error message 2 - empty list", {
+  expect_error(
+    bfactor_interpret(list())
+  )}
+)
+
+test_that("bfactor_interpret error message 3 - NA test 1", {
+  expect_error(
+    bfactor_interpret(NA)
+  )}
+)
+
+test_that("bfactor_interpret error message 3 - NA test 2", {
+  expect_error(
+    bfactor_interpret(NA, scale = "jeffreys" )
+  )}
+)
+
+test_that("bfactor_interpret error message 3 - NaN", {
+  expect_error(
+    bfactor_interpret(NaN)
+  )}
+)
+
+test_that("bfactor_interpret error message 4 - factor", {
+  expect_error(
+    bfactor_interpret(factor(10))
+  )}
+)
+
+test_that("bfactor_interpret error message 4 - char", {
+  expect_error(
+    bfactor_interpret("10")
+  )}
+)
+
+test_that("bfactor_interpret error message 4 - list", {
+  expect_error(
+    bfactor_interpret(list(10))
+  )}
+)
+
+test_that("bfactor_interpret error message 5", {
+  expect_error(
+    bfactor_interpret(-0.6)
+  )}
+)
+
+test_that("bfactor_interpret NA warning", {
+  expect_warning(
+    bfactor_interpret(c(10, NA))
+  )}
+)
+
+context("bfactor_interpret kass-raftery")
+
+test_that("bfactor_interpre kass-rafteryt test 1", {
+  expect_equal(
+    bfactor_interpret(c(0, 2, 4, 21, 151), scale = "kass-raftery"),
+    c("Negative", "Weak", "Positive", "Strong", "Very Strong"))
+  }
+)
+
+test_that("bfactor_interpret kass-raftery test 2", {
+  expect_equal(
+    bfactor_interpret(c(0.99, 2.99, 19.99, 149, 1510), scale = "kass-raftery"),
+    c("Negative", "Weak", "Positive", "Strong", "Very Strong"))
+}
+)
+
+test_that("bfactor_interpret kass-raftery decisive", {
+  expect_equal(
+    bfactor_interpret(200, scale = "kass-raftery"),
+    "Very Strong"
+  )
+})
+
+test_that("bfactor_interpret kass-raftery error message 1", {
+  expect_error(
+    bfactor_interpret(NULL, scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery error message 2 - empty vector", {
+  expect_error(
+    bfactor_interpret(vector(), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery error message 2 - empty list", {
+  expect_error(
+    bfactor_interpret(list(), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery error message 3 - NA", {
+  expect_error(
+    bfactor_interpret(NA, scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery error message 3 - NaN", {
+  expect_error(
+    bfactor_interpret(NaN, scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery error message 4 - factor", {
+  expect_error(
+    bfactor_interpret(factor(10), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery error message 4 - char", {
+  expect_error(
+    bfactor_interpret("10", scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery error message 4 - list", {
+  expect_error(
+    bfactor_interpret(list(10), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery error message 5", {
+  expect_error(
+    bfactor_interpret(-0.6, scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_interpret kass-raftery NA warning", {
+  expect_warning(
+    bfactor_interpret(c(10, NA), scale = "kass-raftery")
+  )}
+)
---
+++ tests/testthat/test-bfactor_log_interpret.R
@@ -0,0 +1,258 @@
+
+context("bfactor_log_interpret Jeffreys")
+
+test_that("bfactor_log_interpret test 1", {
+  expect_equal(
+    bfactor_log_interpret(c(-3.10, -1.78, 1.06, -1.40, 1.21, 0.89, -2.37, 1.23, -8.88, 3.81, -8.38, 0.62), base = 10),
+    c(
+      "Negative",
+      "Negative",
+      "Strong",
+      "Negative",
+      "Strong",
+      "Substantial",
+      "Negative",
+      "Strong",
+      "Negative",
+      "Decisive",
+      "Negative",
+      "Substantial"
+    )
+  )
+})
+
+test_that("bfactor_log_interpret test 2", {
+  expect_equal(
+    bfactor_log_interpret(c(0.07, 1.29, 1.32, -0.62, -1.78, 1.55, -3.02, 1.25, 1.48), base = 10),
+    c(
+      "Weak",
+      "Strong",
+      "Strong",
+      "Negative",
+      "Negative",
+      "Very Strong",
+      "Negative",
+      "Strong",
+      "Strong"
+    )
+  )
+})
+
+test_that("bfactor_log_interpret test 3", {
+    expect_equal(
+    bfactor_log_interpret(c(0.07, 1.29, 1.32, -0.62, -1.78, 1.55, -3.02, 1.25, 1.48), base = 10),
+    bfactor_log_interpret(c(0.07, 1.29, 1.32, -0.62, -1.78, 1.55, -3.02, 1.25, 1.48), base = 10, scale = "jeffreys")
+  )
+})
+
+test_that("bfactor_log_interpret test 4", {
+  expect_error(
+    expect_equal(
+      bfactor_log_interpret(c(0.07, 1.29, 1.32, -0.62, -1.78, 1.55, -3.02, 1.25, 1.48), base = 10, scale = "kass-raftery"),
+      bfactor_log_interpret(c(0.07, 1.29, 1.32, -0.62, -1.78, 1.55, -3.02, 1.25, 1.48), base = 10, scale = "jeffreys")
+      )
+    )
+})
+
+test_that("bfactor_log_interpret error message 1", {
+  expect_error(
+    bfactor_log_interpret(NULL)
+  )}
+)
+
+test_that("bfactor_log_interpret error message 2 - empty vector", {
+  expect_error(
+    bfactor_log_interpret(vector())
+  )}
+)
+
+test_that("bfactor_log_interpret error message 2 - empty list", {
+  expect_error(
+    bfactor_log_interpret(list())
+  )}
+)
+
+test_that("bfactor_log_interpret error message 3 - NA", {
+  expect_error(
+    bfactor_log_interpret(NA)
+  )}
+)
+
+test_that("bfactor_log_interpret error message 3 - NaN", {
+  expect_error(
+    bfactor_log_interpret(NaN)
+  )}
+)
+
+test_that("bfactor_log_interpret error message 4 - factor", {
+  expect_error(
+    bfactor_log_interpret(factor(10))
+  )}
+)
+
+test_that("bfactor_log_interpret error message 4 - char", {
+  expect_error(
+    bfactor_log_interpret("10")
+  )}
+)
+
+test_that("bfactor_log_interpret error message 4 - list", {
+  expect_error(
+    bfactor_log_interpret(list(10))
+  )}
+)
+
+test_that("bfactor_log_interpret base arg NA warning", {
+  expect_warning(
+    bfactor_log_interpret(c(10, NA))
+  )}
+)
+
+test_that("bfactor_log_interpret base arg error message 1", {
+  expect_error(
+    bfactor_log_interpret(bf = .2, base = NULL))
+  }
+)
+
+test_that("bfactor_log_interpret base arg error message 2", {
+  expect_error(
+    bfactor_log_interpret(bf = .2, base = NA)
+  )}
+)
+
+test_that("bfactor_log_interpret base arg error message 3", {
+  expect_error(
+    bfactor_log_interpret(bf = .2, base = c(10, 10))
+  )}
+)
+
+test_that("bfactor_log_interpret base arg error message 4", {
+  expect_error(
+    bfactor_log_interpret(bf = .2, base = factor(10))
+  )}
+)
+
+test_that("bfactor_log_interpret base arg error message 5", {
+  expect_error(
+    bfactor_log_interpret(bf = .2, base = "10")
+  )}
+)
+
+context("bfactor_log_interpret, k-r")
+
+test_that("bfactor_log_interpret error message 1", {
+  expect_error(
+    bfactor_log_interpret(NULL, scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret error message 2 - empty vector", {
+  expect_error(
+    bfactor_log_interpret(vector(), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret error message 2 - empty list", {
+  expect_error(
+    bfactor_log_interpret(list(), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret error message 3 - NA", {
+  expect_error(
+    bfactor_log_interpret(NA, scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret error message 3 - NaN", {
+  expect_error(
+    bfactor_log_interpret(NaN, scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret error message 4 - factor", {
+  expect_error(
+    bfactor_log_interpret(factor(10), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret error message 4 - char", {
+  expect_error(
+    bfactor_log_interpret("10", scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret error message 4 - list", {
+  expect_error(
+    bfactor_log_interpret(list(10), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret base arg NA warning", {
+  expect_warning(
+    bfactor_log_interpret(c(10, NA), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret base arg error message 1", {
+  expect_error(bfactor_log_interpret(lbf = .2, base = NULL, scale = "kass-raftery"))}
+)
+
+test_that("bfactor_log_interpret base arg error message 2", {
+  expect_error(
+    bfactor_log_interpret(lbf = .2, base = NA, scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret base arg error message 3", {
+  expect_error(
+    bfactor_log_interpret(lbf = .2, base = c(10, 10), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret base arg error message 4", {
+  expect_error(
+    bfactor_log_interpret(lbf = .2, base = factor(10), scale = "kass-raftery")
+  )}
+)
+
+test_that("bfactor_log_interpret base arg error message 5", {
+  expect_error(
+    bfactor_log_interpret(lbf = .2, base = "10", scale = "kass-raftery")
+  )}
+)
+
+context("test scale")
+
+test_that("test scale 1", {
+  expect_equal(
+    bfactor_log_interpret(1.23, scale = "kass-raftery"),
+    bfactor_log_interpret(1.23, scale = "kass-RAftery")
+  )}
+)
+
+test_that("test scale 2", {
+  expect_equal(
+    bfactor_log_interpret(1.23, scale = "Kass-raftery"),
+    bfactor_log_interpret(1.23, scale = "Kass-Raftery")
+  )}
+)
+
+test_that("test scale 3", {
+  expect_equal(
+    bfactor_log_interpret(1.23, scale = "kass-raftery"),
+    bfactor_log_interpret(1.23, scale = "Kass-Raftery")
+  )}
+)
+
+test_that("test scale 4", {
+  expect_equal(
+    bfactor_log_interpret(1.23, scale = "Jeffreys"),
+    bfactor_log_interpret(1.23, scale = "jeffreys")
+  )}
+)
+
+
+
+
+
