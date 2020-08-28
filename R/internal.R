791a0ed2c313a02264217ca5aed2fde2bb26864f
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-28 05:15:25 +0100

    add release_questiosn to internal functions

--- R/internal.R
+++ R/internal.R
@@ -348,6 +348,18 @@ check_scale <- function(scale){
   }
 }
 
+# nocov start
+
+release_questions <- function(){
+
+  c(
+    "Have you updated the version number in inst/CITATION (two fields)?",
+    "Have you run all the tests listed in cran-comments.md?",
+    "Have you evaluated the impact on downstream dependencies?"
+  )
+}
+
+# nocov end
 
 
 
