ae5ed937a2cc7160f6527eaca70c1a85598af78b
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-08-28 16:12:00 +0100

    update package documentation

--- R/pcal.R
+++ R/pcal.R
@@ -1,13 +1,14 @@
 
-#' @importFrom Rdpack reprompt
-
-#' @aliases pcal-package
 #' @docType package
+#'
+#' @title Package 'pcal'
 #'
 #' @description Calibrate p-values under a robust perspective using the methods developed by \insertCite{sellke2001;textual}{pcal} and obtain measures of the evidence provided by the data in favor of point null hypotheses which are safer and more straightforward to interpret.
 #'
 #' @details For a complete list of functions see `library(help = "pcal")`. For instructions and examples see the "Usage" section of the [README](https://github.com/pedro-teles-fonseca/pcal/blob/master/README.md).
 #'
+#' @importFrom Rdpack reprompt
+#' @aliases pcal-package
 #' @keywords internal
 
 "_PACKAGE"
