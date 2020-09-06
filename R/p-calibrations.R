e2c360b6a1810a0495c44646ad2f499fa0cbfa0b
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-09-06 20:13:06 +0100

    Improve documentation

--- R/p-calibrations.R
+++ R/p-calibrations.R
@@ -1,24 +1,23 @@
 
 #' @title Lower Bounds on Bayes Factors for Point Null Hypotheses
 #'
-#' @description Calibrate p-values under a robust Bayesian perspective so that they can be interpreted as lower bounds on Bayes factors in favor of point null hypotheses.
+#' @description \loadmathjax Calibrate p-values under a robust Bayesian perspective so that they can be interpreted as lower bounds on Bayes factors in favor of point null hypotheses.
 #'
 #' @param p A numeric vector with values in the \[0,1\] interval.
 #'
-#' @details `bcal` uses the calibration of p-values into lower bounds of Bayes factors developed in \insertCite{sellke2001;textual}{pcal}: \deqn{B(p) = -e \, p \, log (p)}{B(p) = -e p log (p)} for `p` < (1/e) and \deqn{B(p) = 1} otherwise, where `p` is a  p-value on a classical test statistic and \eqn{B(p)} approximates the smallest Bayes factor that is found by changing the prior distribution of the parameter of interest (under the alternative hypothesis) over wide classes of distributions.
+#' @details `bcal` uses the calibration of p-values into lower bounds of Bayes factors developed in \insertCite{sellke2001;textual}{pcal}: \mjdeqn{\operatorname{B}(p) = -\operatorname{e} \, p \, \operatorname{log} (p)}{B(p) = -e p log (p)} for \mjeqn{p < (1/\operatorname{e})}{p < (1/e)} and \mjdeqn{\operatorname{B}(p) = 1}{B(p) = 1} otherwise, where \mjeqn{p}{p} is a  p-value on a classical test statistic and \mjeqn{\operatorname{B}(p)}{B(p)} approximates the smallest Bayes factor that is found by changing the prior distribution of the parameter of interest (under the alternative hypothesis) over wide classes of distributions.
 #'
 #' \insertCite{sellke2001;textual}{pcal} noted that a scenario in which they definitely recommend this calibration is when investigating fit to the null model/hypothesis with no explicit alternative in mind. \insertCite{pericchiTorres2011;textual}{pcal} warn that despite the usefulness and appropriateness of this p-value calibration it does not depend on sample size and hence the lower bounds obtained with large samples may be conservative.
 #'
-#' @return `bcal` returns a numeric vector with the same \code{\link[base]{length}} as `p`.
+#' @return `bcal` returns a numeric vector with the same \code{\link[base]{length}} as `p` and throws a warning message if there are `NA` or `NaN` values in `p`.
 #'
 #' @references
 #' \insertAllCited{}
 #'
 #' @seealso
 #' * \code{\link[pcal]{bfactor_interpret}} for the interpretation of Bayes factors.
-#' * \code{\link[pcal]{bfactor_log_interpret}} for the interpretation of the logarithms of Bayes factors.
 #' * \code{\link[pcal]{bfactor_to_prob}} to turn Bayes factors into posterior probabilities.
-#' * \code{\link[pcal]{pcal}} for a p-value calibration that returns lower bounds on the posterior probabilities of point null hypotheses.
+#' * \code{\link[pcal]{pcal}} for a p-value calibration that returns lower bounds for the posterior probabilities of point null hypotheses.
 #'
 #' @examples
 #' # Calibration of a typical "threshold" p-value:
@@ -61,26 +60,26 @@ bcal <- function(p) {
   }
 
   ifelse(p == 0, 0,
-         ifelse(p < (exp(1) ^ (-1)), -exp(1) * log(p) * p,
+         ifelse(p < (exp(1) ^ (-1)), - exp(1) * p * log(p),
                 1))
 }
 
-#' @title Lower Bounds on the Posterior Probabilities of Point Null Hypotheses
+#' @title Lower Bounds on Posterior Probabilities for Point Null Hypotheses
 #'
-#' @description Calibrate p-values under a robust perspective so that they can be interpreted as either lower bounds on the posterior probabilities of point null hypotheses or as lower bounds on the probabilities of type I errors.
+#' @description \loadmathjax Calibrate p-values under a robust perspective so that they can be directly interpreted as either lower bounds on the posterior probabilities of point null hypotheses or lower bounds on the probabilities of type I errors.
 #'
-#' @param p A numeric vector with values in the \[0,1\] interval.
 #' @param prior_prob A numeric vector with values in the \[0,1\] interval. If `length(p) == 1` then `prior_prob` can be of any positive \code{\link[base]{length}}, but if `length(p) > 1` then the \code{\link[base]{length}} of `prior_prob` can only be `1` or equal to the \code{\link[base]{length}} of `p`.
+#' @inheritParams bcal
 #'
-#' @details \insertCite{sellke2001;textual}{pcal} developed a calibration of p-values into lower bounds for the posterior probabilities of point null hypotheses or lower bounds for the probabilities of type I errors for the case when both the null and the alternative hypotheses have 0.5 prior probability. `pcal` generalizes the aforementioned calibration for prior probabilities other than 0.5.
+#' @details \insertCite{sellke2001;textual}{pcal} developed a calibration of p-values into lower bounds for the posterior probabilities of point null hypotheses (or lower bounds for the probabilities of type I errors): \mjdeqn{\alpha(p) = (1 + \lbrack -e \,  p \operatorname{log}(p) \rbrack^{-1})^{-1}}{alpha(p) = (1 + \[-e p log(p)\]^(-1))^(-1)} where \mjeqn{p}{p} is a  p-value on a classical test statistic. This calibration assumes that both the null and the alternative hypotheses have 0.5 prior probability. `pcal` generalizes the aforementioned calibration for prior probabilities other than 0.5: \mjdeqn{\alpha(p) = \left(1 + \frac{1 - \pi_0}{\pi_0} \, \lbrack -e \,  p \operatorname{log}(p) \rbrack^{-1}\right)^{-1}}{alpha(p) = (1 + (1 - pi_0) / pi_0 \[ -e p log(p) \]^(-1))^(-1)} where \mjeqn{\pi_0}{pi_0} is the prior probability of the null hypothesis. Note that `pcal(p, prior_prob)` is equivalent to \code{\link[pcal]{bfactor_to_prob}(\link[pcal]{bcal}(p), prior_prob)}.
 #'
-#' `pcal` starts by transforming the values in `p` into lower bounds for Bayes factors and then uses those Bayes factors to update the prior probabilities  in `prior_prob` to posterior probabilities (equivalent to \code{\link[pcal]{bfactor_to_prob}(\link[pcal]{bcal}(p), prior_prob)}). For each element of `p`, `pcal` returns an  approximation of the smallest posterior probability of the null hypothesis that is found by changing the prior distribution of the parameter of interest (under the alternative hypothesis) over wide classes of distributions.
+#' For each element of `p`, `pcal` returns an  approximation of the smallest posterior probability of the null hypothesis that is found by changing the prior distribution of the parameter of interest (under the alternative hypothesis) over wide classes of distributions.  Alternatively, the output of `pcal` can also be interpreted as lower bounds on the probabilities of type I errors, which means that this calibration has both Bayesian and Frequentist interpretations.
 #'
 #' The `prior_prob` argument is optional and is set to 0.5 by default, implying prior equiprobability of hypotheses. `prior_prob` can only be of \code{\link[base]{length}} equal to  the `length` of `p`, in which case each prior probability in `prior_prob` is used in the calibration of the corresponding element of `p`, or of \code{\link[base]{length}} `1`, in which case it will be recycled (if `length(p) > 1`) and the same `prior_prob` value is used in the calibration of all the elements of `p`.
 #'
-#' The output of `pcal` can also be interpreted as lower bounds on the probabilities of type I errors. Note that the output of this calibration has both Bayesian and Frequentist interpretations. \insertCite{sellke2001;textual}{pcal} noted that a scenario in which they definitely recommend this calibration is when investigating fit to the null model with no explicit alternative in mind. \insertCite{pericchiTorres2011;textual}{pcal} warn that despite the usefulness and appropriateness of this p-value calibration it does not depend on sample size, and hence the lower bounds obtained with large samples may be conservative.
+#' \insertCite{sellke2001;textual}{pcal} noted that a scenario in which they definitely recommend this calibration is when investigating fit to the null model with no explicit alternative in mind. \insertCite{pericchiTorres2011;textual}{pcal} warn that despite the usefulness and appropriateness of this p-value calibration it does not depend on sample size, and hence the lower bounds obtained with large samples may be conservative.
 #'
-#' @return If `length(p) > 1` then `pcal` returns a numeric vector with the same \code{\link[base]{length}} as `p`, otherwise it returns a numeric vector with the same \code{\link[base]{length}} as `prior_prob`.
+#' @return If `length(p) > 1` then `pcal` returns a numeric vector with the same \code{\link[base]{length}} as `p`, otherwise it returns a numeric vector with the same \code{\link[base]{length}} as `prior_prob`. Warning messages are thrown if there are `NA` or `NaN` values in `p` or `prior_prob`.
 #'
 #' @references
 #' \insertAllCited{}
@@ -157,7 +156,7 @@ pcal <- function(p, prior_prob = 0.5) {
   }
 
   lb_bf <-  ifelse(p == 0, 0,
-                   ifelse(p < (exp(1) ^ (-1)), -exp(1) * log(p) * p,
+                   ifelse(p < (exp(1) ^ (-1)), - exp(1) * p * log(p),
                           1))
 
   (1 + (1 - prior_prob) / prior_prob * (1 / lb_bf)) ^ (-1)
