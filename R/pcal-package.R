3bf4aa3d7de5ab4cdc1ae6b4559c4382ef0b9b5c
Pedro Teles da Fonseca <pedro.teles.fonseca@outlook.com>
2020-09-08 15:40:35 +0100

    Improve pkg documentation

--- R/pcal-package.R
+++ R/pcal-package.R
@@ -4,13 +4,16 @@
 #'
 #' @title pcal: Calibration of P-values for Point Null Hypotheses
 #'
-#' @description Calibrate p-values under a robust perspective using the methods developed by \insertCite{sellke2001;textual}{pcal} and obtain measures of the evidence provided by the data in favor of point null hypotheses which are safer and more straightforward to interpret.
+#' @description Calibrate p-values under a robust perspective using the methods
+#' developed by \insertCite{sellke2001;textual}{pcal} and obtain measures of
+#' the evidence provided by the data in favor of point null hypotheses which
+#' are safer and more straightforward to interpret.
 #'
-#' @details For a complete list of functions see `library(help = "pcal")`. For instructions and examples see the "Usage" section of the [README](https://github.com/pedro-teles-fonseca/pcal/blob/master/README.md).
+#' @details For a complete list of functions see `library(help = "pcal")`.
+#' For instructions and examples see the "Usage" section of the
+#' [README](https://github.com/pedro-teles-fonseca/pcal/blob/master/README.md).
 #'
 #' @aliases pcal-package
 #' @keywords internal
 
 "_PACKAGE"
-
-
